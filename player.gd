31288df3729bc6bf4ba638d932b4b90355c41047
cjgayle1 <cjgayle1@yahoo.com>
2024-10-02 15:22:02 -0500

    Movement restrictions

--- player.gd
+++ player.gd
@@ -9,13 +9,13 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_up",true) or Input.is_action_pressed("move_up",true):
 		$AnimatedSprite2D.play("up")
 		velocity.y = -SPEED
-	if Input.is_action_pressed("ui_down",true) or Input.is_action_pressed("move_down",true):
+	elif Input.is_action_pressed("ui_down",true) or Input.is_action_pressed("move_down",true):
 		$AnimatedSprite2D.play("down")
 		velocity.y = SPEED
-	if Input.is_action_pressed("ui_left",true) or Input.is_action_pressed("move_left",true):
+	elif Input.is_action_pressed("ui_left",true) or Input.is_action_pressed("move_left",true):
 		$AnimatedSprite2D.play("left")
 		velocity.x = -SPEED
-	if Input.is_action_pressed("ui_right",true) or Input.is_action_pressed("move_right",true):
+	elif Input.is_action_pressed("ui_right",true) or Input.is_action_pressed("move_right",true):
 		$AnimatedSprite2D.play("right")
 		velocity.x = SPEED
 	if velocity.length() > 0:

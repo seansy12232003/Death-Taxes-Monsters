eb10fac564c2cb3e01b9513d4ff4e74418a8c031
cjgayle1 <cjgayle1@yahoo.com>
2024-10-07 13:20:57 -0500

    Slow on stairs and houses / trees. New grid layers.

--- main.gd
+++ main.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var slow_speed = 20.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +10,13 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed timea since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_stairs_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		body.set_speed(slow_speed)
+
+
+func _on_stairs_body_exited(body: Node2D) -> void:
+	if body.name == "Player":
+		body.reset_speed()

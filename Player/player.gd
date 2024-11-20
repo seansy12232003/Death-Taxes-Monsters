62731739f26f07d059fa9d12279de92a7266374a
cjgayle1 <cjgayle1@yahoo.com>
2024-11-20 12:50:27 -0600

    changes type ish

--- Player/player.gd
+++ Player/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 var screensize
 const ORIGINAL_SPEED = 75.0
 var SPEED = 75.0
+var can_move: bool = true
 
 
 func _ready():
@@ -12,43 +13,50 @@ func _ready():
 		$"Level/RichTextLabel".text = "Level " + str(Game.selectedMonsters[0]["Level"]) + "  " + str(Game.selectedMonsters[0]["Exp"]) + "/" + str(Game.selectedMonsters[0]["MaxExp"])
 
 func _physics_process(delta):
-	if get_parent().name != "Player":
-		if not %Level1.is_playing():
-			%Level1.play()
-		if not %birds.is_playing():
-			%birds.play()
-		if %BattleMusic.is_playing():
-			%BattleMusic.stop()
-		velocity = Vector2.ZERO # The player's movement vector
-		if Input.is_action_pressed("ui_up",true) or Input.is_action_pressed("move_up",true):
-			$AnimatedSprite2D.play("up")
-			velocity.y = -SPEED
-			if not %Footsteps.is_playing():
-				%Footsteps.play()
-		elif Input.is_action_pressed("ui_down",true) or Input.is_action_pressed("move_down",true):
-			$AnimatedSprite2D.play("down")
-			velocity.y = SPEED
-			if not %Footsteps.is_playing():
-				%Footsteps.play()
-		elif Input.is_action_pressed("ui_left",true) or Input.is_action_pressed("move_left",true):
-			$AnimatedSprite2D.play("left")
-			velocity.x = -SPEED
-			if not %Footsteps.is_playing():
-				%Footsteps.play()
-		elif Input.is_action_pressed("ui_right",true) or Input.is_action_pressed("move_right",true):
-			$AnimatedSprite2D.play("right")
-			velocity.x = SPEED
-			if not %Footsteps.is_playing():
-				%Footsteps.play()
-		if velocity.length() > 0:
-			move_and_slide()
-		else:
-			$AnimatedSprite2D.stop()
+	if can_move:
+		if get_parent().name != "Player":
+			if not %Level1.is_playing():
+				%Level1.play()
+			if not %birds.is_playing():
+				%birds.play()
+			if %BattleMusic.is_playing():
+				%BattleMusic.stop()
+			velocity = Vector2.ZERO # The player's movement vector
+			if Input.is_action_pressed("ui_up",true) or Input.is_action_pressed("move_up",true):
+				$AnimatedSprite2D.play("up")
+				velocity.y = -SPEED
+				if not %Footsteps.is_playing():
+					%Footsteps.play()
+			elif Input.is_action_pressed("ui_down",true) or Input.is_action_pressed("move_down",true):
+				$AnimatedSprite2D.play("down")
+				velocity.y = SPEED
+				if not %Footsteps.is_playing():
+					%Footsteps.play()
+			elif Input.is_action_pressed("ui_left",true) or Input.is_action_pressed("move_left",true):
+				$AnimatedSprite2D.play("left")
+				velocity.x = -SPEED
+				if not %Footsteps.is_playing():
+					%Footsteps.play()
+			elif Input.is_action_pressed("ui_right",true) or Input.is_action_pressed("move_right",true):
+				$AnimatedSprite2D.play("right")
+				velocity.x = SPEED
+				if not %Footsteps.is_playing():
+					%Footsteps.play()
+			if velocity.length() > 0:
+				move_and_slide()
+			else:
+				$AnimatedSprite2D.stop()
+	else:
+		stop_player()
 	
 	# prevent player from going outside the screen
 	#position.x = clamp(position.x, 0, screensize.x)
 	#position.y = clamp(position.y, 0, screensize.y)
 	screensize = get_viewport_rect().size
+
+func stop_player():
+	$AnimatedSprite2D.stop()
+	%Footsteps.stop()
 
 func set_pos(pos): # function to set position of player
 	position = pos
---
+++ camera_swing_city.gd
@@ -0,0 +1,53 @@
+extends Area2D
+
+@export var zoom_out_factor: Vector2 = Vector2(2, 2)  # How much the camera should zoom out
+@export var pan_duration: float = 2.0  # Duration of the pan
+@export var return_duration: float = 1.0  # Duration to return to the original position
+@export var preview_target_position: Vector2 = Vector2()  # The target position for the preview
+
+
+var original_zoom: Vector2
+var original_position: Vector2
+var camera: Camera2D
+var player: Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.has_node("Camera2D2"):
+		player = body
+		camera = body.get_node("Camera2D2")
+		original_zoom = camera.zoom
+		original_position = camera.position
+		disable_player_movement()
+		preview_map()
+		
+		
+func disable_player_movement():
+	print("can move method tirggered")
+	player.can_move = false
+
+func enable_player_movement():
+	print("back to move")
+	player.can_move = true
+
+func preview_map():
+	var tween = get_tree().create_tween()
+	tween.tween_property(camera, "zoom", zoom_out_factor, pan_duration).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(camera, "position", preview_target_position, pan_duration).set_ease(Tween.EASE_IN_OUT)
+	
+	tween.tween_interval(1.0)
+	tween.tween_property(camera, "position", original_position, return_duration).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(camera, "zoom", original_zoom, return_duration).set_ease(Tween.EASE_IN_OUT)
+	
+	tween.tween_callback(Callable(self, "enable_player_movement"))
+	tween.tween_callback(Callable(self, "queue_free"))

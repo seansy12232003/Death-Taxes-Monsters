54aa1e2991d0fe7e02e06ae7bbf3f84168109c4e
cjgayle1 <cjgayle1@yahoo.com>
2024-12-04 15:05:55 -0600

    wtv i chng

--- npc_1.gd
+++ npc_1.gd
@@ -47,7 +47,7 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 
 
 func _on_timer_timeout() -> void:
-	get_node("../../../Dialogue/DialogueBox/Panel/Label").text = "Woof!!!!!!!!!!"
+	get_node("../../../Dialogue/DialogueBox/Panel/Label").text = "Woof"
 	$Bark.play()
 	$Timer.stop()
 
